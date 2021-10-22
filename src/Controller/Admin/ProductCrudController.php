a3401ebc90d22856ee159736850777d3a150b89f
Martin Gladis <martin.gladis@outlook.com>
2021-10-22 23:28:24 +0200

    add filter to EasyAdmin

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,17 +3,33 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class ProductCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Produkt')
+            ->setEntityLabelInPlural('Produkty')
+            ->setSearchFields(['name']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('category'));
     }
 
     public function configureFields(string $pageName): iterable
