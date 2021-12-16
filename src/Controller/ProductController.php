4dd27a30312242de78a2d234e36a60b3adc2ab5e
Martin Gladis <martin.gladis@outlook.com>
2021-12-16 19:43:13 +0100

    Optymise category link in product site and customise some front-end

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use App\Repository\CategoryRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,16 +13,14 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{slug}", name="product")
      */
-    public function index(string $slug = '', ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
+    public function index(string $slug = '', ProductRepository $productRepository): Response
     {
         $product = $productRepository->findOneBy(['slug' => $slug]);
         if (!$product) {
             return $this->render('product/no_product.html.twig');
         }
-        $category = $categoryRepository->findOneBy(['id' => $product->getCategory()]);
         return $this->render('product/index.html.twig', [
             'product' => $product,
-            'category' => $category,
         ]);
     }
 }
