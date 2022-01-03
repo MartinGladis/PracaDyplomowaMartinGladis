cbad439b5c3ebf770cfb0dd8194970ffeddfab06
Martin Gladis <martin.gladis@outlook.com>
2022-01-03 18:47:01 +0100

    Repair some Issues. Add few product. Update composer

--- public/assets/js/calibration.js
+++ public/assets/js/calibration.js
@@ -1,1 +1,1 @@
-alert('Naciskaj na poniższe przyciski w celu kalibracji odczytu ruchek gałek ocznych (Nie zapomnij zezwolić na obraz z kamery');
+alert('Naciskaj na poniższe przyciski w celu kalibracji odczytu ruchek gałek ocznych (Nie zapomnij zezwolić na obraz z kamery)');
