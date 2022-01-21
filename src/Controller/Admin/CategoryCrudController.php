864bf4ccfc9604aeb7db5d6b3185650b030834d4
Martin Gladis <martin.gladis@outlook.com>
2022-01-21 21:31:29 +0100

    Change entity label to polish

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,8 +16,13 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-
-
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Kategoria')
+            ->setEntityLabelInPlural('Kategorie')
+            ->setSearchFields(['name']);
+    }
 
     public function configureFields(string $pageName): iterable
     {
