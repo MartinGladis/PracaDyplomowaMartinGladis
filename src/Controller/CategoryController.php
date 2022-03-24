4eeff34f3d02363852a3fcdf94e0d488aeb1d1f8
Martin Gladis <martin.gladis@outlook.com>
2022-03-24 12:54:49 +0100

    Remove unused namespaces

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Repository\CategoryRepository;
-use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
