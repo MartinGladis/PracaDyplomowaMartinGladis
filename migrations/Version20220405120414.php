eddc5aa5686b3abd525912487efd723578432f1f
Martin Gladis <martin.gladis@outlook.com>
2022-04-05 14:24:38 +0200

    Add migration and remove unused

--- migrations/Version20220405120414.php
+++ migrations/Version20220405120414.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211122153610 extends AbstractMigration
+final class Version20220405120414 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,12 @@ final class Version20211122153610 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD image_filename VARCHAR(255) DEFAULT NULL');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE product DROP image_filename');
     }
 }
